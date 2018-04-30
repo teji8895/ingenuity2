3d228351e6c81f08e2fe8e00a892b74d17f3c6bd
teji8895 <34793215+teji8895@users.noreply.github.com>
2018-04-30 12:06:27 +0530

    ye lo

---
+++ src/Header.js
@@ -0,0 +1,67 @@
+import React,{Component} from 'react';
+
+import {nav,button,form,span} from 'mdbootstrap';
+
+
+class NavDrawer extends Component
+{
+    render(){
+
+  
+    return(
+
+
+<nav className="navbar navbar-expand-lg navbar-dark primary-color">
+
+  
+    <a className="navbar-brand" href="#">Navbar</a>
+
+    
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav" aria-controls="basicExampleNav"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+    </button>
+
+  
+    <div className="collapse navbar-collapse" id="basicExampleNav">
+
+     
+        <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+                <a className="nav-link" href="#">Home
+                    <span className="sr-only">(current)</span>
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+            </li>
+
+         
+            <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div className="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                    <a className="dropdown-item" href="#">Action</a>
+                    <a className="dropdown-item" href="#">Another action</a>
+                    <a className="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+
+        </ul>
+       
+
+        <form className="form-inline">
+            <div className="md-form mt-0">
+                <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
+            </div>
+        </form>
+    </div>
+
+
+</nav>);
+    
+}
+}
+export default NavDrawer;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NavDrawer from './Header'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<NavDrawer/>, document.getElementById('root'));
 registerServiceWorker();
