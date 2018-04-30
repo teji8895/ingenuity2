81a00dbcf38a7f54ae207dbfb95831d82da71e16
teji8895 <34793215+teji8895@users.noreply.github.com>
2018-04-30 21:30:31 +0530

    over to you

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import NavDrawer from './Header'
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 ReactDOM.render(<NavDrawer/>, document.getElementById('root'));
 registerServiceWorker();
